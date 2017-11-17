e95ca61b80529e371c7af14af99c67d7a320b880
ThomasHuai <1624389686@qq.com>
2017-11-17 14:44:48 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,8 +1,8 @@
 model = new KerasJS.Model({
   filepaths: {
-    model: 'cnn.json',
-    weights: 'cnn_weights.buf',
-    metadata: 'cnn_metadata.json',
+    model: './cnn.json',
+    weights: './cnn_weights.buf',
+    metadata: './cnn_metadata.json',
   },
   gpu: false,
   transferLayerOutputs: true
