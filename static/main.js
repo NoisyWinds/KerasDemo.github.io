60431d8f66345d7400de5b9bf0c6442886108e75
ThomasHuai <1624389686@qq.com>
2017-11-17 14:33:28 +0800

    static

---
+++ static/main.js
@@ -0,0 +1,9 @@
+model = new KerasJS.Model({
+  filepaths: {
+    model: 'cnn.json',
+    weights: 'cnn_weights.buf',
+    metadata: 'cnn_metadata.json',
+  },
+  gpu: false,
+  transferLayerOutputs: true
+});
